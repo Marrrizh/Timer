587d0f53498bb5ee3a2ef6a2cd5ff4ca898fa360
Maria S <mariz87@yandex.ru>
2025-12-23 12:39:51 +0300

    Found out all elements of timer

---
+++ script.js
@@ -0,0 +1,10 @@
+const minutesInput=document.querySelector('.minutes-input');
+const secondsInput=document.querySelector('.seconds-input');
+const Minutes=document.querySelector('.minutes');
+const Seconds=document.querySelector('.seconds');
+const Display=document.querySelector('.display');
+const timerFinished=document.querySelector('.timer-finished');
+const startButton=document.querySelector('.start');
+const pauseButton=document.querySelector('.pause');
+const сlearButton=document.querySelector('.clear');
+
